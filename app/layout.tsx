b1ee4065032ce5d6c867c1e6c62a1ce60b0c246f
rachidjarray <rachid.jarray@1stdigital.com>
2024-04-09 14:25:56 +0800

    Update footer. Init

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,11 +48,10 @@ export default function RootLayout({
 							<Link
 								isExternal
 								className="flex items-center gap-1 text-current"
-								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
 								title="nextui.org homepage"
 							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
+								<span className="text-default-600">Made with ❤️ by</span>
+								<p className="text-primary">Lucide</p>
 							</Link>
 						</footer>
 					</div>
